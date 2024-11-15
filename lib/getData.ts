338e5db9de00546a94b078b6c854350ee32565d9
Sayer Tindall <73977638+sayerrrr@users.noreply.github.com>
2024-11-15 14:35:11 -0500

    add CF pages

--- lib/getData.ts
+++ lib/getData.ts
@@ -1,14 +1,11 @@
-import { join } from "path";
-import { readFile } from "fs/promises";
-
 export const getData = async () => {
-  const article_path = join(process.cwd(), "public", "article.json");
-  const eval_path = join(process.cwd(), "public", "evaluation.json");
-  const req_path = join(process.cwd(), "public", "requirements.json");
+  const articleResponse = await fetch("/article.json");
+  const evaluationResponse = await fetch("/evaluation.json");
+  const requirementsResponse = await fetch("/requirements.json");
 
-  const article = JSON.parse(await readFile(article_path, "utf8"));
-  const evaluation = JSON.parse(await readFile(eval_path, "utf8"));
-  const requirements = JSON.parse(await readFile(req_path, "utf8"));
+  const article = await articleResponse.json();
+  const evaluation = await evaluationResponse.json();
+  const requirements = await requirementsResponse.json();
 
   return { article, evaluation, requirements };
 };
